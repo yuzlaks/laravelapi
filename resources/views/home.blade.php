524d15712f201231c4c98c7e77769ad47100b7c9
yuzlaks <yuzyus268@gmail.com>
2019-08-30 10:01:09 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,6 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        tes
                     You are logged in!
                 </div>
             </div>
